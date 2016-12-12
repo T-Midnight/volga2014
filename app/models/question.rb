59ce25cc4cd80bcc61d84f9985c6d1afe07d70e0
T-Midnight <belousova@uchi.ru>
2016-12-12 17:34:09 +0400

    Add search by questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question < ActiveRecord::Base
   validates :message,  presence: true
   has_many :answers, dependent: :destroy
   belongs_to :user
+  include PgSearch
+  pg_search_scope :search_everywhere, against: [:title]
 end
