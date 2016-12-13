d17171645316949340dabaca8b39c8725b46b268
T-Midnight <belousova@uchi.ru>
2016-12-13 17:02:02 +0400

    Useful answer first

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,11 @@
 class QuestionsController < ApplicationController
+
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.all.order vote: :desc
   end
 
   # GET /questions/1
